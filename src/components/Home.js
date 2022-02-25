8496dca30bda94e9d0f3ad177214cf956644f60f
Sunil <snilavarath@gmail.com>
2022-02-25 11:03:22 -0500

    homepage done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
+import Image from 'react-bootstrap/Image'
+
 
 function Home() {
-  return <h1>Welcome to Sunil's Gym</h1>;
+  return (
+    <>
+    <div className = "homeContainer">     
+      <h1 className="brand-logo">Welcome To</h1>
+    </div> 
+    <div className = "homeContainer">     
+      <img 
+        src = "https://i.pinimg.com/564x/54/cf/1e/54cf1e2aa2680092d0d18eb4cc5f9d73.jpg" 
+        alt="Gym Logo" 
+        className = "homeImage"
+      />
+    </div>
+    </>
+  )
 }
 
 export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,19 +2,19 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import Navbar from 'react-bootstrap/Navbar'
 import { Container } from "react-bootstrap";
-// import Nav from "react-bootstrap/Nav"
-
+import Image from 'react-bootstrap/Image'
 
 function NavBar() {
   return (
     <Navbar bg="dark" variant="dark">
       <Navbar.Brand href="#home">
-        <img
+        <Image
           src="https://i.pinimg.com/474x/82/7b/3b/827b3bfcc8bc92d96e208a25592186b1.jpg"
           width="75"
           height="75"
           className="d-inline-block align-top"
           alt="Sunil's Gym Logo"
+          roundedCircle
           />
       </Navbar.Brand>
       <Navbar.Brand href="#home">Sunil's Gym</Navbar.Brand>
