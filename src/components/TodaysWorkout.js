90bbab9cb6255d7d4b15a064f3450307f6f28f89
Sunil <snilavarath@gmail.com>
2022-02-25 12:24:10 -0500

    todays complete

--- src/components/TodaysWorkout.js
+++ src/components/TodaysWorkout.js
@@ -4,17 +4,25 @@ import { Row, Container } from "react-bootstrap";
 
 function TodaysWorkout({ exercises, onChangeToday }) {
   return (
-    <Container fluid={"lg"}>
-      <Row xs={3} md={4} className="g-4">
-        {exercises.map((exercise) => (
-          <ExerciseCard
-            key={exercise.id}
-            exercise={exercise}
-            onChangeToday={onChangeToday}
-          />
-        ))}
-      </Row>
-    </Container>
+    <>
+    <div>
+      <h1>Crush Today's Workout!</h1>
+    </div>
+    <div>
+      <Container fluid={"lg"}>
+        <Row xs={3} md={4} className="g-4">
+          {exercises.map((exercise) => (
+            <ExerciseCard
+              key={exercise.id}
+              exercise={exercise}
+              onChangeToday={onChangeToday}
+              className="card"
+            />
+          ))}
+        </Row>
+      </Container>
+    </div>
+    </>
   );
 }
 
